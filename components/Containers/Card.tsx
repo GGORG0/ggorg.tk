35b5a0662dfb5255e21c7e86feb7bd27609f233f
GGORG <ggorg@onet.pl>
2022-05-07 12:19:14 +0200

    🐛 The children error is finally fixed!

--- components/Containers/Card.tsx
+++ components/Containers/Card.tsx
@@ -10,25 +10,34 @@ interface CardProps {
 }
 
 export default function Card({ children, title = "", textOnly = true, href = "", hrefLocal = false }: CardProps) {
+  console.log(children);
   return (
     <div className="bg-gray-600 m-1 md:m-2 p-1 md:p-3 rounded-xl drop-shadow-lg hover:scale-105 transition-all">
-      {title && <p className="flex justify-center text-xl md:text-2xl font-bold mb-1 drop-shadow-lg">{title}
-        {href !== "" && <p className="text-xl md:text-2xl font-bold drop-shadow-lg ml-2">
-          {hrefLocal && <Link href={href}><a><FaLink /></a></Link>}
-          {!hrefLocal && <a href={href}><FaLink /></a>}
-        </p> }
-      </p>}
+      {title &&
+        <p className="flex justify-center text-xl md:text-2xl font-bold mb-1 drop-shadow-lg">{title}
+          {
+            href !== ""
+              ?
+                <p className="text-xl md:text-2xl font-bold drop-shadow-lg ml-2">
+                  {
+                    hrefLocal
+                      ? <Link href={href}><a><FaLink /></a></Link>
+                      : <a href={href}><FaLink /></a>
+                  }
+                </p>
+              : <></>
+          }
+        </p>
+      }
       {' '}
       <div className="flex justify-center items-center text-l md:text-xl h-max">
         {
-          textOnly && 
-          <p className="text-center">
-            {children}
-          </p>
-        }
-        {
-          !textOnly && 
-          {children}
+          textOnly
+            ?
+            <p className="text-center">
+              {children}
+            </p>
+            : children
         }
       </div>
     </div>
