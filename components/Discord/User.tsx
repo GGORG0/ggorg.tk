5906c9dbf60e811aed01ec5c2467d1e50bcfc90a
GGORG <ggorg@onet.pl>
2022-05-07 11:51:21 +0200

    🐛 Fixed User props being false

--- components/Discord/User.tsx
+++ components/Discord/User.tsx
@@ -13,26 +13,33 @@ export default function User({ userId }: UserProps) {
   });
   return (
     <>
-    <Card textOnly={false}>
-      <div className="flex items-center justify-center w-64 h-64">
-        {lanyard.isValidating && <Spinner />}
-        {!lanyard.isValidating && lanyard.error && <div>!</div>}
-        {!lanyard.isValidating && !lanyard.error &&
-          <>
-            <div className="relative w-64 h-64">
-              <Image
-                src={`https://cdn.discordapp.com/avatars/${lanyard.data.data.discord_user.id}/${lanyard.data.data.discord_user.avatar}.png?size=512`}
-                className="rounded-full"
-                layout="fill"
-                alt="pfp"
-              />
-            </div>
+      <Card textOnly={false}>
+        <div className="flex items-center justify-center w-64 h-64">
+          {lanyard.isValidating
+            ? <Spinner />
+            :
+            (
+              lanyard.error
+                ? <div>!</div>
+                :
+                (
+                  <>
+                    <div className="relative w-64 h-64">
+                      <Image
+                        src={`https://cdn.discordapp.com/avatars/${lanyard.data.data.discord_user.id}/${lanyard.data.data.discord_user.avatar}.png?size=512`}
+                        className="rounded-full"
+                        layout="fill"
+                        alt="pfp"
+                      />
+                    </div>
 
-            <p>{lanyard.data.data.discord_user.username}</p>
-          </>
-        }
-      </div>
-    </Card>
+                    <p>{lanyard.data.data.discord_user.username}</p>
+                  </>
+                )
+            )
+          }
+        </div>
+      </Card>
     </>
   )
 }
