51c86c23ec477c6435c0eb1eff0c8879ceb16495
GGORG <ggorg@onet.pl>
2022-05-07 12:48:19 +0200

    📱 Changed grid layout

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -9,7 +9,7 @@ export default function Friends() {
         <title>GGORG - Friends</title>
       </Head>
       <Page pfp={false} title="Friends">
-        <div className="grid grid-rows-5 md:grid-rows-3 grid-cols-1 md:grid-cols-3 gap-1 md:gap-2">
+        <div className="grid grid-rows-3 md:grid-rows-2 grid-cols-2 md:grid-cols-3 gap-1 md:gap-2">
           {
             process.env.NEXT_PUBLIC_FRIENDS.split(" ").map((value) => (
               <User userId={value} key={value} />
